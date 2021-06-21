fc0162c3572168e2823f451eeb9fbfd39e54ba13
ldex <laurent.duveau+git@gmail.com>
2021-06-21 13:50:42 -0400

    step 2

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.interface';
 
 @Component({
   selector: 'app-product-list', // <app-product-list></app-product-list>
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  title = 'Products';
-  products;
+  title: string = 'Products';
+  products: Product[];
 
   constructor() {
     this.products = [
