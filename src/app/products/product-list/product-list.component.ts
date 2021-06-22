1775d09391f98b20a050eebda9e174a835418c6f
ldex <laurent.duveau+git@gmail.com>
2021-06-22 10:24:54 -0400

    Pagination et erreurs

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -11,7 +13,29 @@ export class ProductListComponent implements OnInit {
 
   title: string = 'Products';
   products: Product[];
+  products$: Observable<Product[]>;
   selectedProduct: Product;
+  errorMessage: string;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
@@ -22,7 +46,24 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.products$ = this
+                        .productService
+                        .products$
+                        .pipe(
+                          catchError(
+                            error => {
+                              this.errorMessage = error;
+                              return EMPTY;
+                            }
+                          )
+                        );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     results => this.products = results
+    //   )
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://storerestservice.azurewebsites.net/api/products/'
 };
